33f1c7c8a7691e5651098a6c32ddb4b9ba099290
iyop45 <roy.miles12@gmail.com>
2016-08-24 09:49:10 +0100

    Create JsonManager.php

---
+++ src/AppBundle/Services/JsonManager.php
@@ -0,0 +1,13 @@
+<?php
+// src/AppBundle/Services/JsonManager.php
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace AppBundle\Services;
+class JsonManager
+{   
+    public function error($message){}
+    public function success($message){}
+}
